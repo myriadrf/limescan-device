c89049d457db19bf42cc865928df4174bd5ee2fb
Shane Hudson <shane@shanehudson.net>
2018-07-20 11:35:11 +0100

    Initial version of limescan-device

---
+++ limescan-device.py
@@ -0,0 +1,51 @@
+import sys
+import requests
+import json
+import subprocess
+import csv
+from datetime import datetime
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+url = config["DATA_URL"] + "write?db=limemicro"
+configurl = config["API_URL"]
+devicename = config["DEVICE_NAME"]
+
+deviceconfig = json.loads(requests.get(configurl + "devices/" + devicename).text)[0]
+command = "LimeScan"
+first_timestamp = None
+last_timestamp = None
+
+if deviceconfig['scan_type'] == "limescan":
+    command = "LimeScan"
+else:
+    command = "LimeMon"
+
+
+filename = devicename
+subprocess.Popen([command + " -f 600M:1000M -C 0 -A LNAW -w 35M -r 16M -OSR 8 -b 512 -g 48 -n 64 -O 'scan-output' -T 1"], shell=True).wait()
+
+with open('scan-output/scan-outputPk.csv', newline='') as csvfile:
+
+    reader = csv.reader(csvfile, delimiter=',')
+    reader.__next__() #first row is filename, skip it
+    i = 1
+    lines = ""
+    for items in reader:
+        timestamp_obj = datetime.strptime(items[0].strip() + " " + items[1].strip(), '%Y-%m-%d %H:%M:%S')
+        nanoseconds = str(round(timestamp_obj.timestamp() * 1e9) + i)
+        if first_timestamp is None:
+            first_timestamp = nanoseconds
+        freqLow = str(items[2].strip())
+        freqHigh = str(items[3].strip())
+        freqStep = str(items[4].strip())
+        dB = '"' + ",".join([str(item).strip() for item in items[6:]]) + '"'
+
+        influxline = "spectrum,sensor=" + devicename + " hzlow=" + freqLow + ",hzhigh=" + freqHigh + ",step=" + freqStep + ",samples=3,dbs=" + dB + " " + nanoseconds
+        lines += '\n' + influxline
+        last_timestamp = nanoseconds
+        i += 1
+    influx_response = requests.post(url, data=lines)
+    sqlite_response = requests.post(configurl + "scans", json = {"device_config_id": deviceconfig['device_config_id'], "scan_start_time": first_timestamp, "scan_finish_time": last_timestamp })
