d18f881249d2ee84e1d079e8d66f399afa47b08c
Shane Hudson <shane@shanehudson.net>
2018-12-20 16:42:46 +0000

    Improved GSM and Power scans

--- limescan-device.py
+++ limescan-device.py
@@ -6,6 +6,8 @@ import csv
 import os
 from datetime import datetime
 import configparser
+from random import randint
+
 
 def CheckForUpdate (workingDir):
     # Check how far ahead master is
@@ -42,45 +44,95 @@ def LimeScan (url, configurl, devicename, deviceconfig):
             freqStep = str(items[4].strip())
             dB = '"' + ",".join([str(item).strip() for item in items[6:]]) + '"'
 
-            influxline = "spectrum,sensor=" + devicename + " hzlow=" + freqLow + ",hzhigh=" + freqHigh + ",step=" + freqStep + ",samples=3,dbs=" + dB + " " + nanoseconds
+            influxline = "power,sensor=" + devicename + " hzlow=" + freqLow + ",hzhigh=" + freqHigh + ",step=" + freqStep + ",samples=3,dbs=" + dB + " " + nanoseconds
             lines += '\n' + influxline
             last_timestamp = nanoseconds
             i += 1
-        influx_response = requests.post(url, data=lines)
         sqlite_response = requests.post(configurl + "scans", json = {"device_config_id": deviceconfig['device_config_id'], "scan_start_time": first_timestamp, "scan_finish_time": last_timestamp })
+        influxlines = ""
+        scanid = json.loads(sqlite_response.text)['stmt']['lastID']
+        for line in lines.split('\n'):
+            columns = line.split(' ')
+            if (len(columns) > 1):
+                columns[-2] = str(columns[-2]) + ',scanid=' + str(scanid)
+                influxlines += ' '.join(columns) + '\n'
+        influx_response = requests.post(url, data=influxlines)
+        #print(influxlines)
 
-def CellScanner (url, configurl, devicename, deviceconfig):
-    if deviceconfig['custom_config'] is None:
-        params = ""
-    else:
-        params = deviceconfig['custom_config']
-    output = subprocess.Popen(["grgsm_scanner " + params], shell=True).wait()
-    print(output)
+
+def GSM (url, configurl, devicename, deviceconfig):
+    band = "GSM900"
+    if deviceconfig['scan_band'] is not None:
+        band = deviceconfig['scan_band']
+    params = "--args rtl -b" + band
+
+    output = subprocess.Popen(["grgsm_scanner " + params], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = output.communicate()
+    output.wait()
+    print("command:", "grgsm_scanner " + params)
+    print("out:", out)
+    print("error:", err)
 
 
+    # dummy = b'linux; GNU C++ version 6.2.0 20161010; Boost_106100; UHD_003.009.005-0-unknown\nARFCN:   86, Freq:  952.2M, CID:     0, LAC:     0, MCC:   0, MNC:   0, Pwr: -44\nARFCN:   96, Freq:  954.2M, CID:     0, LAC:     0, MCC:   0, MNC:   0, Pwr: -45\nARFCN:  105, Freq:  956.0M, CID: 32857, LAC: 21469, MCC: 234, MNC:  10, Pwr: -' + bytes(str(randint(20,60)), encoding='utf-8') + b'\nARFCN:  105, Freq:  956.0M, CID: 32857, LAC: 21469, MCC: 234, MNC:  30, Pwr: -' + bytes(str(randint(20,60)), encoding='utf-8')
+    #dummy = b'linux; GNU C++ version 6.2.0 20161010; Boost_106100; UHD_003.009.005-0-unknown\n\n'
+    #dummy = out
+    dummysplit = str(dummy, 'utf-8').split('\n')
+    lines = ""
+    items = []
+    first_timestamp = datetime.now().timestamp() * 1e9
+    for item in dummysplit[1:]:
+        subitems = {}
+        print(item)
+        commasplit = item.split(',')
+        commasplit = [i.split(':') for i in commasplit]
+        #print("commasplit: " + commasplit)
+        for i in commasplit:
+            if i != "" and i[0] and i[1]:
+                subitems[i[0].strip()] = i[1].strip()
+        try:
+            if subitems['ARFCN'] and int(subitems['ARFCN']) > 0 and int(subitems['MCC']) > 0 and int(subitems['MNC']) > 0 and (int(subitems['Pwr']) > 0 or int(subitems['Pwr']) < 0):
+                items.append(subitems)
+                current_timestamp = str(round(datetime.now().timestamp() * 1e9))
+                influxline = 'gsm,sensor=' + devicename + ',ARFCN=' + subitems['ARFCN'] + ',CID=' + subitems['CID'] + ',LAC=' + subitems['LAC'] + ',MCC=' + subitems['MCC'] + ',MNC=' + subitems['MNC'] + ',band=' + band + ' Pwr=' + subitems['Pwr'] + " " + current_timestamp
+                lines += '\n' + influxline
+        except:
+            continue
+    last_timestamp = datetime.now().timestamp() * 1e9
+    if len(lines) > 0:
+        sqlite_response = requests.post(configurl + "scans", json = {"device_config_id": deviceconfig['device_config_id'], "scan_start_time": first_timestamp, "scan_finish_time": last_timestamp })
+        influxlines = ""
+        scanid = json.loads(sqlite_response.text)['stmt']['lastID']
+        for line in lines.split('\n'):
+            columns = line.split(' ')
+            if (len(columns) > 1):
+                columns[-2] = str(columns[-2]) + ',scanid=' + str(scanid)
+                influxlines += ' '.join(columns) + '\n'
+        influx_response = requests.post(url, data=influxlines)
+        print(influxlines, influx_response.text)
 gitDir = "./"
-print("*********** Checking for code update **************")
+# print("*********** Checking for code update **************")
 
-if CheckForUpdate(gitDir):
-    print ("Updating...")
-    resetCheck = subprocess.check_output(["git", "--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master"])
-    print ("Update complete. Restarting...")
-    # Closes current process and opens a new one. Sudo currently required to use limesuite.
-    os.execvp("sudo", ["sudo"] + ["python3"] + sys.argv)
+#if CheckForUpdate(gitDir):
+#    print ("Updating...")
+#    resetCheck = subprocess.check_output(["git", "--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master"])
+#    print ("Update complete. Restarting...")
+#    # Closes current process and opens a new one. Sudo currently required to use limesuite.
+#    os.execvp("sudo", ["sudo"] + ["python3"] + sys.argv)
 
 config = configparser.ConfigParser()
 configfile = config.read('config.ini')
 if len(configfile) == 0:
     raise ValueError("Configuration file missing, rename config.example.ini to config.ini")
 
-url = config["DEFAULT"]["DATA_URL"] + "write?db=limemicro"
+url = config["DEFAULT"]["DATA_URL"] + "write?db=limescan"
 configurl = config["DEFAULT"]["API_URL"]
 devicename = config["DEFAULT"]["DEVICE_NAME"]
 
 deviceconfig = json.loads(requests.get(configurl + "devices/" + devicename).text)
 
-if deviceconfig['scan_type'] == "limescan":
+if deviceconfig['scan_type'] == "power":
     LimeScan(url, configurl, devicename, deviceconfig)
 
-if deviceconfig['scan_type'] == "cellscanner":
-    CellScanner(url, configurl, devicename, deviceconfig)
+if deviceconfig['scan_type'] == "gsm":
+    GSM(url, configurl, devicename, deviceconfig)
